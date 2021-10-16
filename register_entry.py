6e8fc5e140f82c6409c0fbaf3d14aeee1503d302
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2021-10-16 21:47:51 +0200

    Minor refactor and cleanup by IntelliJ Pycharm Community

--- register_entry.py
+++ register_entry.py
@@ -19,24 +19,27 @@ else:
     basedir = os.getcwd()
 
 import platform
-global osk #onscreen keyboard
+
+global osk  # onscreen keyboard
 if platform.system() == "Windows":
     osk = "osk"
-else: #only on Linux and must be installed. Will fail on MacOSX
+else:  # only on Linux and must be installed. Will fail on MacOSX
     osk = "onboard"
 
+
 def read_employees():
-    xlsx_path = os.path.join(basedir,"employees.xlsx")
-    #print(xlsx_path)
+    xlsx_path = os.path.join(basedir, "employees.xlsx")
+    # print(xlsx_path)
     wb = load_workbook(xlsx_path)
     ws = wb.worksheets[0]
-    people_list  = [[str(e[0]),str(e[1]),str(e[2])] for e in ws.values if e != (None, None, None)]
+    people_list = [[str(e[0]), str(e[1]), str(e[2])] for e in ws.values if e != (None, None, None)]
 
     return people_list[1:]
 
-def write_employee(id, name,surname):
-    xlsx_path = os.path.join(basedir,"employees.xlsx")
-    #print(xlsx_path)
+
+def write_employee(id, name, surname):
+    xlsx_path = os.path.join(basedir, "employees.xlsx")
+    # print(xlsx_path)
     wb = load_workbook(xlsx_path)
     # Select First Worksheet
     ws = wb.worksheets[0]
@@ -45,9 +48,10 @@ def write_employee(id, name,surname):
     ws.append([id, name, surname])
     wb.save(xlsx_path)
 
-def write_entry(id, name,surname, mask_num, employee):
-    xlsx_path = os.path.join(basedir,"entries.xlsx")
-    #print(xlsx_path)
+
+def write_entry(id, name, surname, mask_num, employee):
+    xlsx_path = os.path.join(basedir, "entries.xlsx")
+    # print(xlsx_path)
     wb = load_workbook(xlsx_path)
     # Select First Worksheet
     ws = wb.worksheets[0]
@@ -56,10 +60,11 @@ def write_entry(id, name,surname, mask_num, employee):
     ws.append([datetime.now(), id, name, surname, mask_num, employee])
     wb.save(xlsx_path)
 
+
 class ExtraDataDialog(Gtk.Dialog):
     def __init__(self, parent):
         super().__init__(title="Select Mask number (2 max.)", transient_for=parent, flags=0)
-        self.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,"None (0)", 0, "1 mask", 1, "2 masks", 2)
+        self.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "None (0)", 0, "1 mask", 1, "2 masks", 2)
 
         self.set_default_size(150, 100)
 
@@ -68,6 +73,13 @@ class ExtraDataDialog(Gtk.Dialog):
         box = self.get_content_area()
         box.add(label)
         self.show_all()
+
+
+def load_store(liststore, lst):
+    liststore.clear()
+    for ref in lst:
+        liststore.append(list(ref))
+
 
 class TreeViewFilterWindow(Gtk.Window):
     def __init__(self):
@@ -80,9 +92,10 @@ class TreeViewFilterWindow(Gtk.Window):
         self.grid.set_row_homogeneous(False)
         self.add(self.grid)
 
-        label_new_entry = Gtk.Label(label="Register with your ID, name and surname if you are an external visitor or if you  register for the first time (set check for emproyee)")
+        label_new_entry = Gtk.Label(
+            label="Register with your ID, name and surname if you are an external visitor or if you  register for the "
+                  "first time (set check-box if you are an employee)")
         label_new_entry.set_halign(Gtk.Align.START)
-
         self.grid.attach(label_new_entry, 0, 0, 7, 1)
 
         label_id = Gtk.Label(label="ID")
@@ -90,18 +103,18 @@ class TreeViewFilterWindow(Gtk.Window):
         label_surname = Gtk.Label(label="Surname")
 
         self.grid.attach(label_id, 1, 1, 2, 1)
-        self.grid.attach_next_to(label_name,label_id, Gtk.PositionType.RIGHT, 2, 1)
-        self.grid.attach_next_to(label_surname,label_name, Gtk.PositionType.RIGHT, 3, 1)
+        self.grid.attach_next_to(label_name, label_id, Gtk.PositionType.RIGHT, 2, 1)
+        self.grid.attach_next_to(label_surname, label_name, Gtk.PositionType.RIGHT, 3, 1)
 
-        self.check_employee = Gtk.CheckButton(label ="Employee")
+        self.check_employee = Gtk.CheckButton(label="Employee")
         self.entry_id = Gtk.Entry(text="", placeholder_text="i.e NIF, NIE, Passport nr, etc.")
         self.entry_name = Gtk.Entry(text="", placeholder_text="One or all first names")
         self.entry_surname = Gtk.Entry(text="", placeholder_text="One or all family names")
 
         self.grid.attach(self.check_employee, 0, 2, 1, 1)
-        self.grid.attach_next_to(self.entry_id,label_id, Gtk.PositionType.BOTTOM, 2, 1)
-        self.grid.attach_next_to(self.entry_name,self.entry_id, Gtk.PositionType.RIGHT, 2, 1)
-        self.grid.attach_next_to(self.entry_surname,self.entry_name, Gtk.PositionType.RIGHT, 3, 1)
+        self.grid.attach_next_to(self.entry_id, label_id, Gtk.PositionType.BOTTOM, 2, 1)
+        self.grid.attach_next_to(self.entry_name, self.entry_id, Gtk.PositionType.RIGHT, 2, 1)
+        self.grid.attach_next_to(self.entry_surname, self.entry_name, Gtk.PositionType.RIGHT, 3, 1)
 
         self.button_new_reg = Gtk.Button(label="New entry (unregistered)")
         self.button_new_reg.connect("clicked", self.on_new_entry_button_clicked)
@@ -110,10 +123,11 @@ class TreeViewFilterWindow(Gtk.Window):
         self.button_osk = Gtk.Button()
         self.button_osk.add(Gtk.Image.new_from_file("keyboard.png"))
         self.button_osk.connect("clicked", self.on_screen_keyboard)
-        #self.grid.attach_next_to(self.button_osk,self.button_new_reg, Gtk.PositionType.RIGHT, 1, 1)
+        # self.grid.attach_next_to(self.button_osk,self.button_new_reg, Gtk.PositionType.RIGHT, 1, 1)
         self.grid.attach(self.button_osk, 7, 3, 1, 1)
 
-        label_employee_entry = Gtk.Label(label="If you have registered before lookup and choose your name from the list below")
+        label_employee_entry = Gtk.Label(
+            label="If you have registered before lookup and choose your name from the list below")
         label_employee_entry.set_halign(Gtk.Align.START)
         self.grid.attach(label_employee_entry, 0, 4, 8, 1)
 
@@ -123,7 +137,7 @@ class TreeViewFilterWindow(Gtk.Window):
 
         # Creating the ListStore model
         self.people_liststore = Gtk.ListStore(str, str, str)
-        self.load_store(self.people_liststore, read_employees())
+        load_store(self.people_liststore, read_employees())
 
         # Creating the filter, feeding it with the liststore model
         self.people_filter = self.people_liststore.filter_new()
@@ -151,21 +165,16 @@ class TreeViewFilterWindow(Gtk.Window):
         self.scrollable_treelist.add(self.treeview)
         self.show_all()
 
-    def load_store(self, liststore, lst):
-        liststore.clear()
-        for ref in lst:
-            liststore.append(list(ref))
-
     def people_filter_func(self, model, iter, data):
 
         current_filter_str = self.filter_text.get_text().strip().lower()
-        if (current_filter_str == ""):
+        if current_filter_str == "":
             return True
         else:
-            text =  (model[iter][0] +  model[iter][1] +  model[iter][2]).lower()
+            text = (model[iter][0] + model[iter][1] + model[iter][2]).lower()
             return text.find(current_filter_str) > -1
 
-    def on_filter_text_changed(self,widget):
+    def on_filter_text_changed(self, widget):
         self.people_filter.refilter()
 
     def info_msg(self, message):
@@ -224,7 +233,7 @@ class TreeViewFilterWindow(Gtk.Window):
         id = self.entry_id.get_text().strip()
         name = self.entry_name.get_text().strip()
         surname = self.entry_surname.get_text().strip()
-        if (id == "" or name == "" or surname == ""):
+        if id == "" or name == "" or surname == "":
             self.error_msg("All fields need to be filled")
         else:
 
@@ -235,7 +244,7 @@ class TreeViewFilterWindow(Gtk.Window):
                 write_entry(id, name, surname, str(mask_num), "yes" if is_employee else "no")
                 if is_employee:
                     write_employee(id, name, surname)
-                    self.load_store(self.people_liststore, read_employees())
+                    load_store(self.people_liststore, read_employees())
 
                 self.info_msg(f"Registered entry of: {name} {surname}")
 
@@ -248,12 +257,13 @@ class TreeViewFilterWindow(Gtk.Window):
         dialog.destroy()
         return response
 
+
 win = TreeViewFilterWindow()
 win.connect("destroy", Gtk.main_quit)
 
 win.maximize()
 win.set_resizable(True)
-#win.fullscreen()
+# win.fullscreen()
 
 win.show_all()
 Gtk.main()
